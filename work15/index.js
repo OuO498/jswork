5728f5ce8a5902cb1387d620b19b67f0b2baaffa
WenTao <Yellow@qq.com>
2020-11-03 19:36:12 +0800

    update

---
+++ work15/index.js
@@ -0,0 +1,21 @@
+ShopCart('cart',  [{
+    name: '机电汉堡',
+    price: 8.0,
+    num: 2
+},
+{
+    name: '机电鸡肉卷',
+    price: 8.0,
+    num: 2
+},
+{
+    name: '汉堡套餐',
+    price: 16.0,
+    num: 3
+},
+{
+    name: '汉堡套餐加可乐',
+    price: 20.0,
+    num:3
+}
+]);
